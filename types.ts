735cb5533932bb981e427b76930483b98575f5fb
Aarjusingh14 <aarju14052005@gmail.com>
2025-09-15 17:59:36 +0530

    Types

---
+++ types.ts
@@ -0,0 +1,24 @@
+export interface PersonalProfile {
+  fullName: string;
+  dob: string;
+  contactNumber: string;
+  address: string;
+  gender: string;
+}
+
+export interface UserProfile {
+  education: string;
+  location: string;
+  skills: string;
+  interests: string;
+}
+
+export interface Internship {
+  id: number;
+  title: string;
+  company: string;
+  location: string;
+  description: string;
+  requiredSkills: string[];
+  sector: string;
+}
